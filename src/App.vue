d7b71451cdc957bbfb1fb5a826f27589bac0b1bd
Nicolay <nic.nic343@ya.ru>
2018-02-28 10:24:01 +0700

    Начало

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col">
+        <router-view/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+  $theme-colors: (
+    "primary": #4ab3e4,
+    "danger": #ff4136
+  );
+
+  @import "node_modules/bootstrap/scss/bootstrap";
+</style>
